7e52736ae3322544f3ec1eabb7e940a46492d40a
JFRashu <jannatulalam2019@gmail.com>
2025-01-14 10:23:06 +0600

    Frontend created initially

--- src/Components/Rashu/RNavBar.jsx
+++ src/Components/Rashu/RNavBar.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { Home, LogIn, User,Salad } from 'lucide-react';
+import React from 'react';
+import { Home, LogIn, User, Salad, Utensils } from 'lucide-react';
+
 export const RNavBar = () => {
   return (
     <div className="bg-blue-200 p-3 rounded-lg flex justify-between items-center mb-0">
-        <Home className="w-6 h-6" />
-        <div className="font-semibold">Meal Management 🏠</div>
-        <div className="flex gap-2">
-          <LogIn className="w-6 h-6" />
-          <User className="w-6 h-6" />
-        </div>
+      <Home className="w-6 h-6" />
+      <div className="flex items-center gap-2">
+      <Utensils className="w-4 h-5"/>
+      <div className="font-semibold">Meal Management </div>
+        <Salad className="w-4 h-5" />
+        
       </div>
-  )
-}
+      <div className="flex gap-2">
+        <LogIn className="w-6 h-6" />
+        <User className="w-6 h-6" />
+      </div>
+    </div>
+  );
+};
+
+export default RNavBar;
